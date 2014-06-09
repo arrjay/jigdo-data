c6de6b0f2543e303d5b59c03aa4538e162a8d772
RJ Bergeron <rbergero@gmail.com>
2014-06-08 23:00:52 -0400

    Added Windows Vista EE CDs

--- microsoft/windows/VISTA/Objects.mk
+++ microsoft/windows/VISTA/Objects.mk
@@ -6,7 +6,11 @@ LRMCFRE_EN_CD3.iso LRMCFRE_EN_CD3/Sources/install3.swm \
 LRMCFRE_EN_CD4.iso LRMCFRE_EN_CD4/Sources/install4.swm \
 LRMCFRE_EN_CD5.iso LRMCFRE_EN_CD5/Sources/install5.swm \
 LRMCXFRE_EN_DVD.iso LRMCXFRE_EN_DVD/sources/install.wim LRMCXFRE_EN_DVD/sources/boot.wim \
-LRMEVOL_EN_DVD.iso LRMEVOL_EN_DVD/sources/install.wim LRMEVOL_EN_DVD/sources/boot.wim
+LRMEVOL_EN_DVD.iso LRMEVOL_EN_DVD/sources/install.wim LRMEVOL_EN_DVD/sources/boot.wim \
+LRMEVOL_EN_CD1.iso LRMEVOL_EN_CD1/sources/boot.wim LRMEVOL_EN_CD1/sources/install.swm \
+LRMEVOL_EN_CD2.iso LRMEVOL_EN_CD2/Sources/install2.swm \
+LRMEVOL_EN_CD3.iso LRMEVOL_EN_CD2/Sources/install3.swm \
+LRMEVOL_EN_CD4.iso LRMEVOL_EN_CD2/Sources/install4.swm
 
 LRMEXVOL_EN_DVD.iso : LRMEXVOL_EN_DVD/sources/install.wim LRMEXVOL_EN_DVD/sources/boot.wim
 LRMCFRE_EN_DVD.iso : LRMCFRE_EN_DVD/sources/install.wim LRMCFRE_EN_DVD/sources/boot.wim
@@ -17,3 +21,7 @@ LRMCFRE_EN_CD4.iso : LRMCFRE_EN_CD4/Sources/install4.swm
 LRMCFRE_EN_CD5.iso : LRMCFRE_EN_CD5/Sources/install5.swm
 LRMCXFRE_EN_DVD.iso : LRMCXFRE_EN_DVD/sources/install.wim LRMCXFRE_EN_DVD/sources/boot.wim
 LRMEVOL_EN_DVD.iso : LRMEVOL_EN_DVD/sources/install.wim LRMEVOL_EN_DVD/sources/boot.wim
+LRMEVOL_EN_CD1.iso : LRMEVOL_EN_CD1/sources/boot.wim LRMEVOL_EN_CD1/sources/install.swm
+LRMEVOL_EN_CD2.iso : LRMEVOL_EN_CD2/Sources/install2.swm
+LRMEVOL_EN_CD3.iso : LRMEVOL_EN_CD2/Sources/install3.swm
+LRMEVOL_EN_CD4.iso : LRMEVOL_EN_CD2/Sources/install4.swm
