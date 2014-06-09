00e7b2251b7e6824965888f0fd9c52856d96dbc5
RJ Bergeron <rbergero@gmail.com>
2014-06-08 23:02:11 -0400

    Annnd correct ISO paths

--- microsoft/windows/VISTA/Objects.mk
+++ microsoft/windows/VISTA/Objects.mk
@@ -9,8 +9,8 @@ LRMCXFRE_EN_DVD.iso LRMCXFRE_EN_DVD/sources/install.wim LRMCXFRE_EN_DVD/sources/
 LRMEVOL_EN_DVD.iso LRMEVOL_EN_DVD/sources/install.wim LRMEVOL_EN_DVD/sources/boot.wim \
 LRMEVOL_EN_CD1.iso LRMEVOL_EN_CD1/sources/boot.wim LRMEVOL_EN_CD1/sources/install.swm \
 LRMEVOL_EN_CD2.iso LRMEVOL_EN_CD2/Sources/install2.swm \
-LRMEVOL_EN_CD3.iso LRMEVOL_EN_CD2/Sources/install3.swm \
-LRMEVOL_EN_CD4.iso LRMEVOL_EN_CD2/Sources/install4.swm
+LRMEVOL_EN_CD3.iso LRMEVOL_EN_CD3/Sources/install3.swm \
+LRMEVOL_EN_CD4.iso LRMEVOL_EN_CD4/Sources/install4.swm
 
 LRMEXVOL_EN_DVD.iso : LRMEXVOL_EN_DVD/sources/install.wim LRMEXVOL_EN_DVD/sources/boot.wim
 LRMCFRE_EN_DVD.iso : LRMCFRE_EN_DVD/sources/install.wim LRMCFRE_EN_DVD/sources/boot.wim
@@ -23,5 +23,5 @@ LRMCXFRE_EN_DVD.iso : LRMCXFRE_EN_DVD/sources/install.wim LRMCXFRE_EN_DVD/source
 LRMEVOL_EN_DVD.iso : LRMEVOL_EN_DVD/sources/install.wim LRMEVOL_EN_DVD/sources/boot.wim
 LRMEVOL_EN_CD1.iso : LRMEVOL_EN_CD1/sources/boot.wim LRMEVOL_EN_CD1/sources/install.swm
 LRMEVOL_EN_CD2.iso : LRMEVOL_EN_CD2/Sources/install2.swm
-LRMEVOL_EN_CD3.iso : LRMEVOL_EN_CD2/Sources/install3.swm
-LRMEVOL_EN_CD4.iso : LRMEVOL_EN_CD2/Sources/install4.swm
+LRMEVOL_EN_CD3.iso : LRMEVOL_EN_CD3/Sources/install3.swm
+LRMEVOL_EN_CD4.iso : LRMEVOL_EN_CD4/Sources/install4.swm
