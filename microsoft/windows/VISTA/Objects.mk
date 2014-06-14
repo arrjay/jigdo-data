640462f820c282e7cc56893e3c40a83df435073b
RJ Bergeron <rbergero@gmail.com>
2014-06-14 13:35:00 -0400

    Start adding Vista SP1 data

--- microsoft/windows/VISTA/Objects.mk
+++ microsoft/windows/VISTA/Objects.mk
@@ -10,7 +10,9 @@ LRMEVOL_EN_DVD.iso LRMEVOL_EN_DVD/sources/install.wim LRMEVOL_EN_DVD/sources/boo
 LRMEVOL_EN_CD1.iso LRMEVOL_EN_CD1/sources/boot.wim LRMEVOL_EN_CD1/sources/install.swm \
 LRMEVOL_EN_CD2.iso LRMEVOL_EN_CD2/Sources/install2.swm \
 LRMEVOL_EN_CD3.iso LRMEVOL_EN_CD3/Sources/install3.swm \
-LRMEVOL_EN_CD4.iso LRMEVOL_EN_CD4/Sources/install4.swm
+LRMEVOL_EN_CD4.iso LRMEVOL_EN_CD4/Sources/install4.swm \
+FRMCFRE_EN_DVD.iso FRMCFRE_EN_DVD/sources/boot.wim FRMCFRE_EN_DVD/sources/install.wim \
+FRMCXFRE_EN_DVD.iso FRMCXFRE_EN_DVD/sources/boot.wim FRMCXFRE_EN_DVD/sources/install.wim
 
 LRMEXVOL_EN_DVD.iso : LRMEXVOL_EN_DVD/sources/install.wim LRMEXVOL_EN_DVD/sources/boot.wim
 LRMCFRE_EN_DVD.iso : LRMCFRE_EN_DVD/sources/install.wim LRMCFRE_EN_DVD/sources/boot.wim
@@ -25,3 +27,5 @@ LRMEVOL_EN_CD1.iso : LRMEVOL_EN_CD1/sources/boot.wim LRMEVOL_EN_CD1/sources/inst
 LRMEVOL_EN_CD2.iso : LRMEVOL_EN_CD2/Sources/install2.swm
 LRMEVOL_EN_CD3.iso : LRMEVOL_EN_CD3/Sources/install3.swm
 LRMEVOL_EN_CD4.iso : LRMEVOL_EN_CD4/Sources/install4.swm
+FRMCFRE_EN_DVD.iso : FRMCFRE_EN_DVD/sources/boot.wim FRMCFRE_EN_DVD/sources/install.wim
+FRMCXFRE_EN_DVD.iso : FRMCXFRE_EN_DVD/sources/boot.wim FRMCXFRE_EN_DVD/sources/install.wim
