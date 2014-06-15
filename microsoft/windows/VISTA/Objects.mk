a2d3306f3a4940540aa044e39b857befd5c615f1
RJ Bergeron <rbergero@gmail.com>
2014-06-15 11:33:20 -0400

    Added vista enterprise sp1

--- microsoft/windows/VISTA/Objects.mk
+++ microsoft/windows/VISTA/Objects.mk
@@ -12,7 +12,9 @@ LRMEVOL_EN_CD2.iso LRMEVOL_EN_CD2/Sources/install2.swm \
 LRMEVOL_EN_CD3.iso LRMEVOL_EN_CD3/Sources/install3.swm \
 LRMEVOL_EN_CD4.iso LRMEVOL_EN_CD4/Sources/install4.swm \
 FRMCFRE_EN_DVD.iso FRMCFRE_EN_DVD/sources/boot.wim FRMCFRE_EN_DVD/sources/install.wim \
-FRMCXFRE_EN_DVD.iso FRMCXFRE_EN_DVD/sources/boot.wim FRMCXFRE_EN_DVD/sources/install.wim
+FRMCXFRE_EN_DVD.iso FRMCXFRE_EN_DVD/sources/boot.wim FRMCXFRE_EN_DVD/sources/install.wim \
+FRMEVOL_EN_DVD.iso FRMEVOL_EN_DVD/sources/boot.wim FRMEVOL_EN_DVD/sources/install.wim \
+FRMEXVOL_EN_DVD.iso FRMEXVOL_EN_DVD/sources/boot.wim FRMEXVOL_EN_DVD/sources/install.wim
 
 LRMEXVOL_EN_DVD.iso : LRMEXVOL_EN_DVD/sources/install.wim LRMEXVOL_EN_DVD/sources/boot.wim
 LRMCFRE_EN_DVD.iso : LRMCFRE_EN_DVD/sources/install.wim LRMCFRE_EN_DVD/sources/boot.wim
@@ -29,3 +31,5 @@ LRMEVOL_EN_CD3.iso : LRMEVOL_EN_CD3/Sources/install3.swm
 LRMEVOL_EN_CD4.iso : LRMEVOL_EN_CD4/Sources/install4.swm
 FRMCFRE_EN_DVD.iso : FRMCFRE_EN_DVD/sources/boot.wim FRMCFRE_EN_DVD/sources/install.wim
 FRMCXFRE_EN_DVD.iso : FRMCXFRE_EN_DVD/sources/boot.wim FRMCXFRE_EN_DVD/sources/install.wim
+FRMEVOL_EN_DVD.iso : FRMEVOL_EN_DVD/sources/boot.wim FRMEVOL_EN_DVD/sources/install.wim
+FRMEXVOL_EN_DVD.iso : FRMEXVOL_EN_DVD/sources/boot.wim FRMEXVOL_EN_DVD/sources/install.wim
