d68248b4426cd454125871c31b0e6c74acb120fc
RJ Bergeron <rbergero@gmail.com>
2014-08-01 11:52:23 -0400

    Added OS X 10.5

---
+++ apple/OS_X/Objects.mk
@@ -0,0 +1,1 @@
+OBJECTS	:= 10.5_Leopard.cdr 10.5_Leopard.udro.dmg 10.5_Leopard_Server.cdr 10.5_Leopard_Server.udro.dmg 10.5_Leopard_ServerAdministrationTools.cdr 10.5_Leopard_ServerAdministrationTools.udro.dmg
