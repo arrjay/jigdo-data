d36b8edb2e0eeb8111674716fa6e9d8c08b13323
RJ Bergeron <rbergero@gmail.com>
2014-09-01 10:59:19 -0400

    added windows vista sp1 (that is, sp1 for vista, not vista with sp1)

--- microsoft/windows/VISTA/Objects.mk
+++ microsoft/windows/VISTA/Objects.mk
@@ -17,7 +17,8 @@ FRMEVOL_EN_DVD.iso FRMEVOL_EN_DVD/sources/boot.wim FRMEVOL_EN_DVD/sources/instal
 FRMEXVOL_EN_DVD.iso FRMEXVOL_EN_DVD/sources/boot.wim FRMEXVOL_EN_DVD/sources/install.wim \
 FRMCNXFRE_EN_DVD.iso FRMCNXFRE_EN_DVD/sources/boot.wim FRMCNXFRE_EN_DVD/sources/install.wim \
 FRMCCHK_EN_DVD.iso FRMCCHK_EN_DVD/sources/boot.wim FRMCCHK_EN_DVD/sources/install.wim \
-FRMCXCHK_EN_DVD.iso FRMCXCHK_EN_DVD/sources/boot.wim FRMCXCHK_EN_DVD/sources/install.wim
+FRMCXCHK_EN_DVD.iso FRMCXCHK_EN_DVD/sources/boot.wim FRMCXCHK_EN_DVD/sources/install.wim \
+FRMCSP1_CD1.iso FRMCSP1_DVD.iso
 
 LRMEXVOL_EN_DVD.iso : LRMEXVOL_EN_DVD/sources/install.wim LRMEXVOL_EN_DVD/sources/boot.wim
 LRMCFRE_EN_DVD.iso : LRMCFRE_EN_DVD/sources/install.wim LRMCFRE_EN_DVD/sources/boot.wim
