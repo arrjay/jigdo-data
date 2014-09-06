0b6f860747c8be14ccecf2a8b08b2bd5737a5db4
RJ Bergeron <rbergero@gmail.com>
2014-09-06 19:53:01 -0400

    chop up windows server 2008 discs

--- microsoft/windows/2008/Objects.mk
+++ microsoft/windows/2008/Objects.mk
@@ -1,1 +1,3 @@
-OBJECTS	:= GRMSXVOL_EN_DVD.iso
+OBJECTS	:= GRMSXVOL_EN_DVD.iso GRMSXVOL_EN_DVD/sources/boot.wim GRMSXVOL_EN_DVD/sources/install.wim
+
+GRMSXVOL_EN_DVD.iso : GRMSXVOL_EN_DVD/sources/boot.wim GRMSXVOL_EN_DVD/sources/install.wim
