7cbd9510af38ff3b3eb49333ebd1cd7b285129c9
RJ Bergeron <rbergero@gmail.com>
2015-03-08 22:19:40 -0400

    added yellowdog 5.0.2 distribution cds

---
+++ 3rdparty/yellowdog/Objects.mk
@@ -0,0 +1,1 @@
+OBJECTS	:= yellowdog-5.0.2-20070627-disc1.iso yellowdog-5.0.2-20070627-disc2.iso yellowdog-5.0.2-20070627-disc3.iso yellowdog-5.0.2-20070627-disc4.iso yellowdog-5.0.2-20070627-disc5.iso yellowdog-5.0.2-20070627-disc6.iso
